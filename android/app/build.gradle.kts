375dde6e091be777b43be647de55141b5e237a8d
Bilal Ahmad <dev.renosoftwares@gmail.com>
2025-07-30 17:02:50 +0530

    deeplinking fully working

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.qayham.qrscanner"
         minSdk = providers.gradleProperty("flutter.minSdkVersion").map(String::toInt).getOrElse(23)
         targetSdk = providers.gradleProperty("flutter.targetSdkVersion").map(String::toInt).getOrElse(35)
-        versionCode = providers.gradleProperty("flutter.versionCode").map(String::toInt).getOrElse(13)
-        versionName = providers.gradleProperty("flutter.versionName").getOrElse("0.1.1")
+        versionCode = providers.gradleProperty("flutter.versionCode").map(String::toInt).getOrElse(15)
+        versionName = providers.gradleProperty("flutter.versionName").getOrElse("0.1.2")
     }
 
     signingConfigs {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_scanner/providers/history_provider.dart';
@@ -10,21 +11,40 @@ void main() async {
   await MobileAds.instance.initialize();
   await AdService.instance.preloadAllAds(); // Preload all ads
 
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_) => HistoryProvider()),
-  ], child: const MyApp()));
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => HistoryProvider()),
+    ],
+    child: const MyApp(),
+  ));
 }
+
+// Define your GoRouter routes
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/products/qrscanner',
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'QR/Barcode Scanner',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomeScreen());
+    return MaterialApp.router(
+      title: 'QR/Barcode Scanner',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routerConfig: _router,
+    );
   }
 }
